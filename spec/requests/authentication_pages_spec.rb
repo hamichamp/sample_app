fe03373b130f9cd097bb34e42305b356ed7fbcd1
Koizumi Masato <hamichan@yahoo.co.jp>
2014-03-14 06:09:54 +0900

    Merge branch 'exercise8'

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 describe "AuthenticationPages" do
-  # describe "GET /authentication_pages" do
-  #   it "works! (now write some real specs)" do
-  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get authentication_pages_index_path
-  #     response.status.should be(200)
-  #   end
-  # end
   subject { page }
 
   describe "signin page" do
@@ -24,7 +17,7 @@ describe "AuthenticationPages" do
   		before { click_button "Sign in" }
 
   		it { should have_selector('title', text:'Sign in') }
-  		it { should have_selector('div.alert.alert-error', text:'Invalid' )}
+      it { should have_error_message('Invalid') }
 
   		describe "after visiting another page" do
   			before { click_link "Home" }
@@ -34,11 +27,7 @@ describe "AuthenticationPages" do
 
   	describe "with valid information" do
   		let(:user) { FactoryGirl.create(:user) }
-  		before do
-  			fill_in "Email", with:user.email.upcase
-  			fill_in "Password", with:user.password
-  			click_button "Sign in"
-  		end
+      before { valid_signin(user) }
 
   		it { should have_selector('title', text: user.name) }
   		it { should have_link('Profile', href: user_path(user)) }
